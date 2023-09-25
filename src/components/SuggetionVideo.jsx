8eba18a2a05fb8cc0729118fbdbec21c44bfa90d
Abdul Salam <iamabdul054@gmail.com>
2023-09-26 04:42:24 +0530

    firstCommit

---
+++ src/components/SuggetionVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuggetionVideo = () => {
+  return (
+    <div>SuggetionVideo</div>
+  )
+}
+
+export default SuggetionVideo
---
+++ src/context/contextApi.js
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResults, setsearchResults] = useState(false);
+  const [selectCategories, setselectCategories] = useState(false);
+  const [mobileMenu, setmobileMenu] = useState(false);
+
+  useEffect(() => {
+    fetchSelectedCategoryData(selectCategories);
+  }, [selectCategories]);
+
+  const fetchSelectedCategoryData = (query) => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+      console.log(contents);
+      setsearchResults(contents);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        setLoading,
+        setsearchResults,
+        setselectCategories,
+        selectCategories,
+        mobileMenu,
+        setmobileMenu,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
